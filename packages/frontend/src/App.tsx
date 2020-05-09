13bacd8572ffcdad98a8758e7b4df613a16592a4
Taekeun Hwang <tkhwang@gmail.com>
2020-05-09 15:20:15 +0900

    Add react menu bar

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,46 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
 import './App.css'
+import ButtonAppBar from './components/ButtonAppBar'
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<Counter />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<span>
-					<span>Learn </span>
-					<a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-						React
-					</a>
-					<span>, </span>
-					<a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-						Redux
-					</a>
-					<span>, </span>
-					<a
-						className="App-link"
-						href="https://redux-toolkit.js.org/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Redux Toolkit
-					</a>
-					,<span> and </span>
-					<a
-						className="App-link"
-						href="https://react-redux.js.org/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						React Redux
-					</a>
-				</span>
-			</header>
+			<ButtonAppBar />
+			<img
+				src="https://raw.githubusercontent.com/tkhwang/tkhwang-etc/master/img/2020/04/photo-1523240795612-9a054b0db644.jpeg"
+				alt="new"
+			/>
 		</div>
 	)
 }
