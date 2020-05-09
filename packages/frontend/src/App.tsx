ec04f0932c1672cfd8c8f4250dca12748b4ee067
Taekeun Hwang <tkhwang@gmail.com>
2020-05-09 15:55:48 +0900

    Add learn-in-public article link in top page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -6,6 +6,20 @@ function App() {
 	return (
 		<div className="App">
 			<ButtonAppBar />
+			<ul>
+				<li>
+					Inspired by{' '}
+					<a href="https://www.swyx.io/writing/learn-in-public/">Learn In Public: The fastest way to learn</a>
+				</li>
+				<li>
+					(Korean translation :{' '}
+					<a href="https://tkhwang.me/2020-04-22-learn-in-public-korean-translation">
+						(번역) 공개적으로 학습하라
+					</a>
+					)
+				</li>
+			</ul>
+
 			<img
 				src="https://raw.githubusercontent.com/tkhwang/tkhwang-etc/master/img/2020/04/photo-1523240795612-9a054b0db644.jpeg"
 				alt="new"
