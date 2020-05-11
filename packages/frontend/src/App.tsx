0142b9bd791b8531305d4d8112d0302e909321e6
Taekeun Hwang <tkhwang@gmail.com>
2020-05-12 00:32:59 +0900

    [fe-landing-page] Front Page updated.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,30 +1,13 @@
 import React from 'react'
 import './App.css'
+import { LandingPage } from './components/LandingPage'
 import ButtonAppBar from './components/ButtonAppBar'
 
 function App() {
 	return (
 		<div className="App">
 			<ButtonAppBar />
-			<ul>
-				<li>
-					Inspired by{' '}
-					<a href="https://www.swyx.io/writing/learn-in-public/">Learn In Public: The fastest way to learn</a>
-				</li>
-				<li>
-					(Korean translation :{' '}
-					<a href="https://tkhwang.me/2020-04-22-learn-in-public-korean-translation">
-						(번역) 공개적으로 학습하라
-					</a>
-					)
-				</li>
-			</ul>
-
-			<img
-				style={{ width: '100%'}}
-				src="https://raw.githubusercontent.com/tkhwang/tkhwang-etc/master/img/2020/04/photo-1523240795612-9a054b0db644.jpeg"
-				alt="new"
-			/>
+			<LandingPage />
 		</div>
 	)
 }
---
+++ packages/frontend/src/components/LandingPage.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+
+export interface ILandingPageProps {}
+
+export function LandingPage(props: ILandingPageProps) {
+	return (
+		<React.Fragment>
+			<ul>
+				<li>
+					Inspired by{' '}
+					<a href="https://www.swyx.io/writing/learn-in-public/">Learn In Public: The fastest way to learn</a>
+				</li>
+				<li>
+					(Korean translation :{' '}
+					<a href="https://tkhwang.me/2020-04-22-learn-in-public-korean-translation">
+						(번역) 공개적으로 학습하라
+					</a>
+					)
+				</li>
+			</ul>
+
+			<img
+				style={{ width: '70%' }}
+				src="https://raw.githubusercontent.com/tkhwang/tkhwang-etc/master/img/2020/04/photo-1523240795612-9a054b0db644.jpeg"
+				alt="new"
+			/>
+		</React.Fragment>
+	)
+}
