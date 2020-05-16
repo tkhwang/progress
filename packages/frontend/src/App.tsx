de07f0b6e4285b3a9eae07db5fa27d6ce7874066
Taekeun Hwang <tkhwang@gmail.com>
2020-05-17 08:15:00 +0900

    Fix lint error

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import './App.css'
-import { LandingPage } from './components/LandingPage'
 import ButtonAppBar from './components/ButtonAppBar'
+import { LandingPage } from './components/LandingPage'
+import './App.css'
 
 function App() {
 	return (
-		<div className="App">
+		<div className='App'>
 			<ButtonAppBar />
 			<LandingPage />
 		</div>
