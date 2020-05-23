16fa4f38f3dd1a409fceea6fc4d3c2bd5dbd265f
Taekeun Hwang <tkhwang@gmail.com>
2020-05-23 21:38:55 +0900

    👌 IMPROVE: Update module alias path config in frontend.

--- packages/frontend/config-overrides.js
+++ packages/frontend/config-overrides.js
@@ -4,7 +4,25 @@ module.exports = function override(config) {
   alias({
     '@progress/api/*': './node_modules/@progress/api/dist/*',
     '@progress/api': './node_modules/@progress/api/dist',
+    'class-transformer': 'node_modules/@progress/api/node_modules/class-transformer',
+    'class-validator': 'node_modules/@progress/api/node_modules/class-validator',
   })(config);
 
   return config;
 };
+
+// TODO: not working. why ?
+/*
+const { alias, configPaths } = require('react-app-rewire-alias');
+
+module.exports = function override(config) {
+  alias(configPaths())(config);
+
+  // or with spread and custom config file
+  alias({
+    ...configPaths('tsconfig.paths.json'),
+  })(config);
+
+  return config;
+};
+*/
