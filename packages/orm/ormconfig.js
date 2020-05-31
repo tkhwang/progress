ef931258e5488f641edf073e068c2423891db9f6
Taekeun Hwang <tkhwang@gmail.com>
2020-06-01 00:13:00 +0900

    👌 IMPROVE: typeorm config updated.

--- packages/orm/ormconfig.js
+++ packages/orm/ormconfig.js
@@ -1,19 +1,20 @@
-const { SnakeNamingStrategy } = require('typeorm-naming-strategies')
-const snake = new SnakeNamingStrategy()
+const { SnakeNamingStrategy } = require('typeorm-naming-strategies');
+const snake = new SnakeNamingStrategy();
 module.exports = {
-	logging: true,
-	type: 'mysql',
-	host: process.env.RDS_TKHWANG_PROGRESS_HOST,
-	port: 3306,
-	username: process.env.RDS_TKHWANG_PROGRESS_USERNAME,
-	password: process.env.RDS_TKHWANG_PROGRESS_PASSWORD,
-	database: process.env.RDS_TKHWANG_PROGRESS_DB,
-	entities: ['./dist/entities/*.js'],
-	migrations: ['./src/migrations/*.ts'],
-	namingStrategy: snake,
-	dateStrings: ['DATE'],
-	cli: {
-		migrationsDir: './src/migrations',
-	},
-	synchronize: true,
-}
+  logging: true,
+  type: 'mysql',
+  host: process.env.RDS_TKHWANG_PROGRESS_HOST,
+  port: 3306,
+  charset: 'UTF8MB4_UNICODE_CI',
+  username: process.env.RDS_TKHWANG_PROGRESS_USERNAME,
+  password: process.env.RDS_TKHWANG_PROGRESS_PASSWORD,
+  database: process.env.RDS_TKHWANG_PROGRESS_DB,
+  entities: ['./dist/entities/*.js'],
+  migrations: ['./src/migrations/*.ts'],
+  namingStrategy: snake,
+  dateStrings: ['DATE'],
+  cli: {
+    migrationsDir: './src/migrations',
+  },
+  synchronize: true,
+};
--- packages/orm/src/ormConfig.ts
+++ packages/orm/src/ormConfig.ts
@@ -1,26 +1,27 @@
+import * as dotenv from 'dotenv'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions'
-import * as dotenv from 'dotenv'
 
 const snake = new SnakeNamingStrategy()
 dotenv.config()
 
 const connectionOptions: MysqlConnectionOptions = {
-	logging: true,
-	type: 'mysql',
-	host: process.env.RDS_TKHWANG_PROGRESS_HOST,
-	port: 3306,
-	username: process.env.RDS_TKHWANG_PROGRESS_USERNAME,
-	password: process.env.RDS_TKHWANG_PROGRESS_PASSWORD,
-	database: process.env.RDS_TKHWANG_PROGRESS_DB,
-	entities: ['./dist/entities/*.js'],
-	migrations: ['./src/migrations/*.ts'],
-	namingStrategy: snake,
-	dateStrings: ['DATE'],
-	// cli: {
-	// 	migrationsDir: './src/migrations',
-	// },
-	synchronize: true,
+  logging: true,
+  type: 'mysql',
+  host: process.env.RDS_TKHWANG_PROGRESS_HOST,
+  port: 3306,
+  charset: 'UTF8MB4_UNICODE_CI',
+  username: process.env.RDS_TKHWANG_PROGRESS_USERNAME,
+  password: process.env.RDS_TKHWANG_PROGRESS_PASSWORD,
+  database: process.env.RDS_TKHWANG_PROGRESS_DB,
+  entities: ['./dist/entities/*.js'],
+  migrations: ['./src/migrations/*.ts'],
+  namingStrategy: snake,
+  dateStrings: ['DATE'],
+  // cli: {
+  // 	migrationsDir: './src/migrations',
+  // },
+  synchronize: true,
 }
 
 export default connectionOptions
