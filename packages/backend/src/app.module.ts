b671631a8e13e777fd01c3a8a0ffa878b2899aa2
Taekeun Hwang <tkhwang@gmail.com>
2020-05-31 17:32:31 +0900

    [add-resource] 🐛 FIX: Weird tsc compilation due to wrong orm import

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,31 +1,32 @@
-import { AuthController } from '@controllers/AuthController'
-import { EtcController } from '@controllers/EtcController'
-import { InterestController } from '@controllers/InterestController'
-import { ResourceController } from '@controllers/ResourceController'
-import { UrlController } from '@controllers/UrlController'
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { JwtModule } from '@nestjs/jwt'
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
-import { Interest, User } from '@progress/orm'
-import connectionOptions from '@progress/orm/ormConfig'
-import { UsersRepository } from '@repositories/UsersRepository'
-import { AuthService } from '@services/AuthService'
-import { GoogleStrategy } from '@services/GoogleStrategy'
-import { InterestService } from '@services/InterestService'
-import { JwtStrategy } from '@services/JwtStrategy'
-import { ResourceService } from '@services/ResourceService'
-import { UrlService } from '@services/UrlService'
-import { UsersService } from '@services/UsersService'
-import { jwtConstants } from '@utils/AuthContstants'
-import 'module-alias/register'
-import { MorganModule } from 'nest-morgan'
-import path from 'path'
-import { Resource } from '../../orm/src/entities/Resource'
-import configuration from './config/configurations'
-import { InterestRepository } from './repositories/InterestRepository'
-import { ResourceRepository } from './repositories/ResourceRepository'
-import { LocalStrategy } from './services/LocalStrategy'
+import { AuthController } from '@controllers/AuthController';
+import { EtcController } from '@controllers/EtcController';
+import { InterestController } from '@controllers/InterestController';
+import { ResourceController } from '@controllers/ResourceController';
+import { UrlController } from '@controllers/UrlController';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Interest, Resource, User } from '@progress/orm';
+import connectionOptions from '@progress/orm/ormConfig';
+import { InterestRepository } from '@repositories/InterestRepository';
+import { ResourceRepository } from '@repositories/ResourceRepository';
+import { UsersRepository } from '@repositories/UsersRepository';
+import { AuthService } from '@services/AuthService';
+import { GoogleStrategy } from '@services/GoogleStrategy';
+import { InterestService } from '@services/InterestService';
+import { JwtStrategy } from '@services/JwtStrategy';
+import { LocalStrategy } from '@services/LocalStrategy';
+import { ResourceService } from '@services/ResourceService';
+import { UrlService } from '@services/UrlService';
+import { UsersService } from '@services/UsersService';
+import { jwtConstants } from '@utils/AuthContstants';
+import 'module-alias/register';
+import { MorganModule } from 'nest-morgan';
+import path from 'path';
+import configuration from './config/configurations';
+
+
 
 @Module({
   imports: [
