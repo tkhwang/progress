a6903c00793b2db08d105b658208eacb69266a31
Taekeun Hwang <tkhwang@gmail.com>
2020-06-10 23:21:18 +0900

    📦 NEW: Add LoggerMiddleware of nest.js

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -4,24 +4,26 @@ import bodyParser from 'body-parser'
 import 'module-alias/register'
 import morgan from 'morgan'
 import { AppModule } from './app.module'
+import { LoggerMiddleware } from './middlwares/LoggerMiddleware'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(
+  const nestApp = await NestFactory.create(AppModule)
+  nestApp.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   )
-  app.use(bodyParser.json())
+  nestApp.use(new LoggerMiddleware().use)
+  nestApp.use(bodyParser.json())
   // app.use(morgan('[:status]:method :url :response-time ms'))
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-  app.enableCors()
+  nestApp.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+  nestApp.enableCors()
 
   const globalPrefix = 'v1'
-  app.setGlobalPrefix(globalPrefix)
+  nestApp.setGlobalPrefix(globalPrefix)
   const port = process.env.PORT || 80
 
-  await app.listen(port, () => {
+  await nestApp.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`Listening at http://localhost:${port}/${globalPrefix}`)
   })
---
+++ packages/backend/src/middlwares/LoggerMiddleware.ts
@@ -0,0 +1,23 @@
+import { Injectable, NestMiddleware } from '@nestjs/common'
+import * as express from 'express'
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(request: express.Request, response: express.Response, next: (err?: any) => any): any {
+    if (process.env.DEBUG) {
+      // tslint:disable-next-line:no-console
+      console.log('url -> ', request.originalUrl)
+      // tslint:disable-next-line:no-console
+      console.log('cookies -> ', request.cookies)
+      // tslint:disable-next-line:no-console
+      console.log('query -> ', request.query)
+      // tslint:disable-next-line:no-console
+      console.log('params -> ', request.params)
+      // tslint:disable-next-line:no-console
+      console.log('body ->', request.body)
+      // tslint:disable-next-line:no-console
+      console.log('header ->', request.headers)
+    }
+    next()
+  }
+}
