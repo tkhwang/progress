bfd425cb9473472a48c96ad0206ddfdf47820e1e
Taekeun Hwang <tkhwang@gmail.com>
2020-06-14 17:52:44 +0900

    📦 NEW: Add new interest UI using button

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useInterests } from 'src/hooks/useInterests'
-import { InterestGetInterestsResult } from '@progress/api'
+import { InterestGetInterestsResult, APIS } from '@progress/api'
 import InterestCard from 'src/components/InterestCard'
 import styled from 'styled-components'
 import { EyeOutlined } from '@ant-design/icons'
 import { UniqueKey } from 'src/services/UniqueKey'
+import { Modal, Input, Button } from 'antd'
+import { AuthService } from 'src/services/AuthService'
+import Title from 'antd/lib/typography/Title'
+import { AiOutlineAppstoreAdd } from 'react-icons/ai'
 
 export interface IHomeProps {
   forceUpdate: (time: string) => void
@@ -18,27 +22,75 @@ const DivFlex = styled.div`
 //   padding: 30px 30px 30px 30px;
 
 export default function Home(props: IHomeProps) {
+  const [interest, setInterest] = useState('')
   const [interests, setInterests] = useInterests()
+  const [modalVisible, setModalVisible] = useState(false)
+  const [uniqueKey, setUniqueKey] = useState(UniqueKey.newKey())
   console.log('Home -> interests', interests)
 
   useEffect(() => {
     props.forceUpdate(UniqueKey.newKey())
   }, [])
 
+  const handleOk = () => {
+    setUniqueKey(UniqueKey.newKey())
+    setModalVisible(false)
+    // setInterests([...interests.filter((d: string) => d !== 'Add new'), interest, 'Add new'])
+  }
+
+  const handleCancel = () => {
+    setUniqueKey(UniqueKey.newKey())
+    setModalVisible(false)
+  }
+
+  const onRegisterInterest = async (int: string) => {
+    const apis = new APIS.Interest()
+    const user: any = AuthService.getCurrentUser()
+
+    try {
+      await apis.postInterest({ interest: int, user: user.id })
+      const newInterest = [{ interest, createdAt: new Date() }]
+
+      setInterests([...interests, ...newInterest])
+    } catch (error) {}
+  }
+
   return (
     <React.Fragment>
       <h1>
         <EyeOutlined /> Interests
       </h1>
-      <DivFlex>
-        {interests.map((interest: InterestGetInterestsResult) => (
+      <div onClick={(e) => setModalVisible(true)}>
+        <Button style={{ width: '30%' }} type="primary">
+          Add New Interest
+        </Button>
+      </div>
+      <DivFlex key={uniqueKey}>
+        {interests.map((i: InterestGetInterestsResult) => (
           <InterestCard
-            key={`${interest.interest}`}
-            interest={`${interest.interest}`}
-            createdAt={`${interest.createdAt}`}
+            key={`${i.interest}`}
+            interest={`${i.interest}`}
+            createdAt={`${i.createdAt}`}
           />
         ))}
       </DivFlex>
+      <Modal
+        title="Add new interest"
+        visible={modalVisible}
+        onOk={() => {
+          onRegisterInterest(interest)
+          handleOk()
+        }}
+        onCancel={handleCancel}
+      >
+        <Input
+          key={uniqueKey}
+          placeholder="Add your interest."
+          onChange={(e: any) => {
+            setInterest(e.target.value)
+          }}
+        />
+      </Modal>
     </React.Fragment>
   )
 }
