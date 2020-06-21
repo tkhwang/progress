51b003f7fb3c40fcc4098cbbeeddbc032c57da64
Taekeun Hwang <tkhwang@gmail.com>
2020-06-21 10:46:49 +0900

    Change ant design css import method

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import RootContext from './stores/RootContext'
+import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
 
 ReactDOM.render(
   <React.StrictMode>
