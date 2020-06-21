9e15d05b74318686e0af51c00fed1c0b7bab084b
Taekeun Hwang <tkhwang@gmail.com>
2020-06-22 00:17:19 +0900

    👌 IMPROVE: puppeteer lauch error fix

--- packages/backend/src/services/UrlService.ts
+++ packages/backend/src/services/UrlService.ts
@@ -12,7 +12,9 @@ export class UrlService {
 
   async toImage(userId: number, url: string): Promise<string | null> {
     try {
-      const browser = await puppeteer.launch()
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      })
       const page = await browser.newPage()
 
       await page.emulate(puppeteer.devices['iPhone 6'])
